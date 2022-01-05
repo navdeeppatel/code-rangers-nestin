e353eb066ad79ffc6116468eaec06ba20af3b46d
DESKTOP-4B1UUEA\Navdeep <savaliyanavdeep123@gmail.com>
2022-01-05 23:33:07 +0530

    base primary structure and color are done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './nestin-merchant/header/header.component';
+import {FlexLayoutModule} from "@angular/flex-layout";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon'
+import {MatSidenavModule} from '@angular/material/sidenav'
+import {MatToolbarModule  } from '@angular/material/toolbar'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FlexLayoutModule,MatIconModule, MatButtonModule, MatSidenavModule, MatToolbarModule,BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
